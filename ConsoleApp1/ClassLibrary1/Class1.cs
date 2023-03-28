965c61b6e3889d91a5cb0256db3d9278c27169c7
Sergey U <serezha.usov.2122@mail.ru>
2023-03-28 12:51:42 +0300

    class library

---
+++ ConsoleApp1/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static List<int> Sequence(int N)
+        {
+            List<int> primes = new List<int>();
+            for (int i = 1; i <= N; i++)
+                primes.Add(i);
+            return primes;
+        }
+        public  void SequenceDisplay(int N)
+        {
+            if (N == 0)
+                Console.WriteLine("N>0!");
+            Console.WriteLine("{0}\n", String.Join(",", Sequence(N)));
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            class1.SequenceDisplay(Convert.ToInt32(Console.ReadLine()));
+        }
+    }
+}
